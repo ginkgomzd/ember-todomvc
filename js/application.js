3afe7a630de089eddaa75c9cb8a04bc4bdb45e35
ginkgoMZD <mzd@ginkgostreet.com>
2014-12-10 14:02:15 -0500

    add links to guide

--- js/application.js
+++ js/application.js
@@ -1,4 +1,11 @@
 
+/***
+ * Ember Getting Started Guide
+ * http://emberjs.com/guides/getting-started/
+ *
+ * Sample implementation of the ToDoMVC Application
+ * http://todomvc.com/
+ ***/
 window.Todos = Ember.Application.create();
 
 Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
