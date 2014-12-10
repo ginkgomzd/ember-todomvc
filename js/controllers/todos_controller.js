e2e2007f6515640aa66777b324dea93ff0f76dff
ginkgoMZD <mzd@ginkgostreet.com>
2014-12-10 13:59:23 -0500

    check-all/uncheck-all functionality

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -43,4 +43,14 @@ Todos.TodosController = Ember.ArrayController.extend({
     var remaining = this.get('remaining');
     return remaining === 1 ? 'item' : 'items';
   }.property('remaining')
+  ,
+  allAreDone: function(key, value) {
+    if (value === undefined) {
+      return !!this.get('length') && this.isEvery('isCompleted');
+    } else {
+      this.setEach('isCompleted', value);
+      this.invoke('save');
+      return value;
+    }
+  }.property('@each.isCompleted')
 });
