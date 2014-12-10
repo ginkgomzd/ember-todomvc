f158fd6ecd09dfa6b27d2bf72ab6a66059b0216a
ginkgoMZD <mzd@ginkgostreet.com>
2014-12-10 11:22:14 -0500

    editing todos: helper view to set focus on add element;

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -1,5 +1,26 @@
 
 Todos.TodoController = Ember.ObjectController.extend({
+  isEditing: false,
+  actions: {
+    editTodo: function() {
+      console.log('TodosController.editTodo');
+      this.set('isEditing', true);
+    },
+    acceptChanges: function() {
+      this.set('isEditing', false);
+
+      if (Ember.isEmpty(this.get('model.title'))) {
+        this.send('removeTodo');
+      } else {
+        this.get('model').save();
+      }
+    },
+    removeTodo: function() {
+      var todo = this.get('model');
+      todo.deleteRecord();
+      todo.save();
+    }
+  },
   isCompleted: function(key, value){
     var model = this.get('model');
 
